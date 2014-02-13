2ae4a89ac3b2284b6caa6bd4afebdaaa39f931bd
tuor0002 <Kayla@Michaelas-MacBook-Pro-2.local>
2014-02-13 08:52:09 -0500

    Added Reviews

--- js/animation.js
+++ js/animation.js
@@ -26,3 +26,23 @@ $('.ships-section').waypoint(function () {
 	$ships.eq(3).addClass('js-ship-delay-2');
 	$ships.addClass('js-ships-animate');
 }, { offset: '60%' });
+
+
+
+
+var dinoData = $.ajax('dinos.json', {
+	
+	dataType: 'json'
+	
+});
+
+
+// Because AJAX take time to download things of the internet
+//.done() will be triggered only after jquery has finished downloading
+dinoData.done(function (data) {
+	var dino =  Math.round (Math.random() * data.length - 1 );
+	
+	$('.dino-name').html(data[dino].name);
+	$('.dino-period').html(data[dino].period);
+	$('.dino-diet').html(data[dino].diet);
+});
